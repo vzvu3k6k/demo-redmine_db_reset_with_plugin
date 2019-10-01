d2af1aba086257331296871c8e29ffddb10712b8
vzvu3k6k <vzvu3k6k@gmail.com>
2019-10-02 06:41:07 +0900

    Just use Rails 4 Migration class

--- overlay/plugins/migration_demo/db/migrate/001_create_foos_table.rb
+++ overlay/plugins/migration_demo/db/migrate/001_create_foos_table.rb
@@ -1,11 +1,4 @@
-base_class =
-  if defined? ActiveRecord::Migration::Current
-    ActiveRecord::Migration::Current
-  else
-    ActiveRecord::Migration
-  end
-
-class CreateFoosTable < base_class
+class CreateFoosTable < ActiveRecord::Migration
   def change
     create_table :foos
   end
